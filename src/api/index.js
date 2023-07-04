ffe000c53fdf20f95797bbbe435a50a7021abbc9
KiraCodes6103 <21je1072@iitism.ac.in>
2023-07-05 00:51:12 +0530

    FirstCommit

---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "X-RapidAPI-Key": "bd24c2402cmshe932a8c89a3fd6dp126344jsne5cf36244814",
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
